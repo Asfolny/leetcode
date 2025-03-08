627456d0835eb2bcb3e85acd7c04b34da24f185a
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-03-08 21:30:22 +0100

    C#: ADD pure LINQ implementation of K Consecutive Black Blocks

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/solution.cs
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/solution.cs
@@ -1,16 +1,7 @@
 public class Solution {
     public int MinimumRecolors(string blocks, int k) {
-        var moves = k+1;
-
-        for(int i = 0; i <= blocks.Length-k; i++) {
-            var part = blocks.Substring(i, k);
-            var needed = part.Count((c) => c == 'W');
-
-            if (moves > needed) {
-                moves = needed;
-            }
-        }
-
-        return moves;
+        return Enumerable.Range(0, blocks.Length - k + 1)
+            .Select(i => blocks.Substring(i, k).Count(c => c == 'W'))
+            .Aggregate(k+1, (acc, m) => m < acc ? m : acc);
     }
 }

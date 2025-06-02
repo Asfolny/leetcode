3563287a4ef855f1fa227585ee81eb01261dc88a
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-06-02 19:29:43 +0200

    C#: ADD Binary Search solution

---
+++ 704-binary-search/solution.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        return RecSearch(nums, 0, nums.Length-1, target);
+    }
+
+    private int RecSearch(int[]nums, int left, int right, int target) {
+        if (left >= right)
+            return nums[left] == target ? left : -1;
+
+        int mid = (left + right) / 2;
+        if (nums[mid] == target)
+            return mid;
+        
+        return nums[mid] > target ?
+            RecSearch(nums, left, mid-1, target) : 
+            RecSearch(nums, mid+1, right, target);
+    }
+}

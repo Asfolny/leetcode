43473c8c987048b5b41dc3becbc6fe555fc877ad
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-11-14 23:10:10 +0100

    JAVA: ADD solution for Combination Sum

---
+++ 39-combination-sum/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        res = new ArrayList<List<Integer>>();
+        var set = new ArrayList<Integer>();
+        dfs(nums, 0, target, set);
+        return res;
+    }
+
+    private void dfs(int[] nums, int index, int target, List<Integer> cur) {
+        if (target == 0) {
+            res.add(new ArrayList(cur));
+            return;
+        }
+
+        if (target < 0 || index >= nums.length) {
+            return;
+        }
+
+        cur.add(nums[index]);
+        dfs(nums, index, target - nums[index], cur);
+        cur.remove(cur.size()-1);
+        dfs(nums, index+1, target, cur);
+    }
+}
